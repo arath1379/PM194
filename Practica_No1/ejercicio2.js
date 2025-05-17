4945afc4bb4762fd2cecab2415d12616a9e6f910
arath1379 <122041683@upq.edu.mx>
2025-05-16 19:54:04 -0600

    Ejercicio 2 Practica No 1

---
+++ Practica_No1/ejercicio2.js
@@ -0,0 +1,10 @@
+//Funcion Tradicional
+//function cuadrado(numero) {
+    //return numero * numero;
+//}
+//console.log(cuadrado(4)); // Ejemplo: imprime 16
+
+//Funcion Arrow
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(5)); // Ejemplo: imprime 25
