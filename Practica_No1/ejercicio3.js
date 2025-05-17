ae85332a155fdf5970e788cc197a37769e424a7c
arath1379 <122041683@upq.edu.mx>
2025-05-16 19:57:32 -0600

    Ejercicio 3 Practica No 1

---
+++ Practica_No1/ejercicio3.js
@@ -0,0 +1,9 @@
+/* crea un arrow function llamada saludoPersonalizado que reciba dos parametros: nombre y edad
+y retorne una cadema como la siguiente */
+
+//"Hola, me llamo Isay y tengo 37 años"
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Isay", 37)); // Ejemplo: imprime "Hola, me llamo Isay y tengo 37 años"
+console.log(saludoPersonalizado("Arath", 20)); // Ejemplo: imprime "Hola, me llamo Arath y tengo 20 años"
