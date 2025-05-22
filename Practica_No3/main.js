8f3649d58275cc3b4d3ae85548212d1a1b93ea87
arath1379 <122041683@upq.edu.mx>
2025-05-21 20:11:16 -0600

    Ejercicio a Practica No 3

---
+++ Practica_No3/main.js
@@ -0,0 +1,9 @@
+// realizamos la importacion del modulo
+const { restar } = require('./utils');
+
+// Pruebas
+console.log(restar(10, 5));    // 5
+console.log(restar(3, 7));     // -4
+console.log(restar(0, 0));     // 0
+console.log(restar(100, 25));  // 75
+console.log(restar(-10, -5));  // -5
