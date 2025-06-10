0204c3a8b6867d8fd7623120a60bbb0ff644fac3
arath1379 <122041683@upq.edu.mx>
2025-06-10 17:13:56 -0600

    Avance Practica 4: Props, contents

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,28 @@
+/* Zona 1 : Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+const Texto=(props)=>{
+  const {contenido}=props;
+  return (
+    <Text>{contenido}</Text>
+  )
+};
+
+/* Zona 2 : Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="React Native"></Texto>
+      <Button title='Trabaja'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* Zona 3 : Estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
