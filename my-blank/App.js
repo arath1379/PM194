751ef4cfebc92ce754f4f770ed40221fa60db431
arath1379 <122041683@upq.edu.mx>
2025-06-11 20:02:30 -0600

    Practica No 4 Finalizada

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,22 +1,36 @@
 /* Zona 1 : Importaciones */
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
-const Texto=(props)=>{
-  const {contenido}=props;
+const Texto = () => {
+  const [contenido, setContenido] = useState('Hola Mundo RNative');
+  const actualizaTexto = () => {
+    setContenido('Estado actualizado del text');
+  };
+  return <Text onPress={actualizaTexto}>{contenido}</Text>;
+};
+
+const Titulo = () => {
+  const [textoBoton, setTextoBoton] = useState('Trabaja');
+  const cambiarTextoBoton = () => {
+    setTextoBoton('¡Texto del botón actualizado!');
+  };
   return (
-    <Text>{contenido}</Text>
-  )
+    <View style={{ alignItems: 'center', marginBottom: 20 }}>
+      <Button title={textoBoton} onPress={cambiarTextoBoton} />
+    </View>
+  );
 };
 
 /* Zona 2 : Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido="Hola"></Texto>
-      <Texto contenido="Mundo"></Texto>
-      <Texto contenido="React Native"></Texto>
-      <Button title='Trabaja'></Button>
+      <Texto />
+      <Titulo />
+      <Texto />
+      <Texto />
       <StatusBar style="auto" />
     </View>
   );
